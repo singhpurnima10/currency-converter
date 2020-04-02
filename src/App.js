15be8609d1b35d1f2b9a6b5cf756def07973e637
Purnima SIngh <spurnima10aug@gmail.com>
2020-04-02 20:37:52 +0530

    formatting of file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { getDataAsync } from "./redux/action";
 import { render } from "react-dom";
 import * as _ from "lodash";
 // import  from "./data"
-import {CurrencyKeyData} from './data';
+import { CurrencyKeyData } from "./data";
 
 import {
   Main,
@@ -22,60 +22,78 @@ import {
   SubHeader
 } from "./App.style";
 
-
 class App extends React.Component {
-  state={
+  state = {
     inputValue: 1,
     outputValue: 1.5601,
     numericValue: 1.5601,
-    currencyText: 'Canadian Dollar'
-  }
+    currencyText: "Canadian Dollar"
+  };
   componentDidMount(props) {
     this.props.getCurrencyData();
   }
-  handleChange=(e)=>{
-    this.setState({outputValue: this.state.inputValue * e.target.value, numericValue: e.target.value, currencyText: e.target.options[e.target.selectedIndex].text});
-  }
-  handleUserInput=(e)=> {
+  handleChange = e => {
+    this.setState({
+      outputValue: this.state.inputValue * e.target.value,
+      numericValue: e.target.value,
+      currencyText: e.target.options[e.target.selectedIndex].text
+    });
+  };
+  handleUserInput = e => {
     const re = /^[0-9\b]+$/;
 
     // if value is not blank, then test the regex
 
-    if (e.target.value === '' || re.test(e.target.value)){
-      this.setState({inputValue : e.target.value},() => {
-        this.setState({outputValue: this.state.inputValue * this.state.numericValue})
+    if (e.target.value === "" || re.test(e.target.value)) {
+      this.setState({ inputValue: e.target.value }, () => {
+        this.setState({
+          outputValue: this.state.inputValue * this.state.numericValue
+        });
       });
     }
-    
-  }
+  };
 
   render() {
     const { currencyData } = this.props;
-    const d = currencyData.date && (new Date(currencyData.date.toString()));
-  const dtf = new Intl.DateTimeFormat('en', { year: 'numeric', month: 'short', day: '2-digit' }); 
-  const [{ value: mo },,{ value: da },,{ value: ye }] = dtf.formatToParts(d); 
+    const d = currencyData.date && new Date(currencyData.date.toString());
+    const dtf = new Intl.DateTimeFormat("en", {
+      year: "numeric",
+      month: "short",
+      day: "2-digit"
+    });
+    const [{ value: mo }, , { value: da }, , { value: ye }] = dtf.formatToParts(
+      d
+    );
     return (
       <Main>
-      <MainContainer>
-        <Header>Currency converter Application</Header>
-          <SubHeader>Rates as per date {da} {mo} {ye} </SubHeader>
-    <CurrencyText>{this.state.inputValue} Euro equals {this.state.outputValue} {this.state.currencyText}</CurrencyText>
-           <InputContainer><Currency type='tel' value={this.state.inputValue} onChange={this.handleUserInput} /> <CurrencyText>EURO</CurrencyText> </InputContainer>
-           
-           <OutputContainer>
-           <Currency type='text' value={this.state.outputValue} />
-           <DropdownWrapper>
-           <Dropdown onChange={(event)=>this.handleChange(event)}>
-           {_.map(currencyData.rates,(data, key) => 
-             <option value={data}>{CurrencyKeyData[key]}</option> 
-           )}
-           </Dropdown>
-           </DropdownWrapper>
-           
-           
-           </OutputContainer>
-           
-      </MainContainer>
+        <MainContainer>
+          <Header>Currency converter Application</Header>
+          <SubHeader>
+            Rates as per date {da} {mo} {ye}{" "}
+          </SubHeader>
+          <CurrencyText>
+            1 Euro equals {this.state.numericValue} {this.state.currencyText}
+          </CurrencyText>
+          <InputContainer>
+            <Currency
+              type="text"
+              value={this.state.inputValue}
+              onChange={this.handleUserInput}
+            />{" "}
+            <CurrencyText>EURO</CurrencyText>{" "}
+          </InputContainer>
+
+          <OutputContainer>
+            <Currency type="text" value={this.state.outputValue} />
+            <DropdownWrapper>
+              <Dropdown onChange={event => this.handleChange(event)}>
+                {_.map(currencyData.rates, (data, key) => (
+                  <option value={data}>{CurrencyKeyData[key]}</option>
+                ))}
+              </Dropdown>
+            </DropdownWrapper>
+          </OutputContainer>
+        </MainContainer>
       </Main>
     );
   }
